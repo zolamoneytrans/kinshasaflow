99d98c6f2bbbd98b6a41039b93b75bbc88d6cdba
emmanuel nduwa <drnduwa@gmail.com>
2026-02-01 13:48:53 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-874039458-d0447",
+  "appId": "1:196367644911:web:74bd118b2cb442b1dc031a",
+  "apiKey": "AIzaSyD_wEf8tEk9ZffJfUnLI7ndITSt5p05FoU",
+  "authDomain": "studio-874039458-d0447.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "196367644911"
+};

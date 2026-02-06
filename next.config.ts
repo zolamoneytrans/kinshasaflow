12ff666c130bd0c04f4b785bf6b3ae3c2d3df6a8
emmanuel nduwa <drnduwa@gmail.com>
2026-02-06 12:47:38 +0000

    i want to deploy the app into production

--- next.config.ts
+++ next.config.ts
@@ -10,12 +10,6 @@ const pwaPlugin = withPWA({
 
 const nextConfig: NextConfig = {
   /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
     remotePatterns: [
       {

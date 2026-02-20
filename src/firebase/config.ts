85bc495f0300f023a1c65c5b0ee8a3a73c36a98e
emmanuel nduwa <drnduwa@gmail.com>
2026-02-20 20:53:30 +0000

    I still have this error

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:196367644911:web:74bd118b2cb442b1dc031a",
   "apiKey": "AIzaSyD_wEf8tEk9ZffJfUnLI7ndITSt5p05FoU",
   "authDomain": "studio-874039458-d0447.firebaseapp.com",
+  "storageBucket": "studio-874039458-d0447.appspot.com",
   "measurementId": "",
   "messagingSenderId": "196367644911"
 };

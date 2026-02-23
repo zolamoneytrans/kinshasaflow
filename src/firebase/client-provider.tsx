78fd1bdc24ab0bd7e44ed83b870efdcc8f3ed875
emmanuel nduwa <drnduwa@gmail.com>
2026-02-23 07:16:34 +0000

    Try fixing this error: `Runtime ChunkLoadError: Loading chunk app/layout

--- src/firebase/client-provider.tsx
+++ src/firebase/client-provider.tsx
@@ -2,7 +2,7 @@
 
 import React, { useMemo, type ReactNode } from 'react';
 import { FirebaseProvider } from '@/firebase/provider';
-import { initializeFirebase } from '@/firebase';
+import { initializeFirebase } from '@/firebase/init';
 
 interface FirebaseClientProviderProps {
   children: ReactNode;

d3d6822a720c65fb90d73634dda72dcb86411394
emmanuel nduwa <drnduwa@gmail.com>
2026-02-25 18:39:00 +0000

    where should i regenerate it from ?

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,24 +1,26 @@
+// Give the service worker access to Firebase Messaging.
+// Note that you can only use Firebase Messaging here. Other Firebase libraries are not available in the service worker.
 importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
 
-// These values are public and safe to include in the service worker.
-// They will be replaced by your actual config during the build process if using Firebase App Hosting,
-// or you can manually paste your firebaseConfig object here if needed.
-const firebaseConfig = {
+// Initialize the Firebase app in the service worker by passing in
+// your app's Firebase config object.
+// You can find these values in your Firebase Console (Project Settings > General).
+firebase.initializeApp({
   apiKey: "AIzaSyD_wEf8tEk9ZffJfUnLI7ndITSt5p05FoU",
   authDomain: "studio-874039458-d0447.firebaseapp.com",
   projectId: "studio-874039458-d0447",
   storageBucket: "studio-874039458-d0447.appspot.com",
   messagingSenderId: "196367644911",
   appId: "1:196367644911:web:74bd118b2cb442b1dc031a"
-};
+});
 
-firebase.initializeApp(firebaseConfig);
-
+// Retrieve an instance of Firebase Messaging so that it can handle background messages.
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
   console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  // Customize notification here
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,

5b993330463d6adf7181bfa25c48394b58f2793b
emmanuel nduwa <drnduwa@gmail.com>
2026-02-25 18:43:43 +0000

    this is my VAPID key : BAZlcLpJe-GN5uU69wylKIepUwkqGCQDAzxWt33bEADJJJLYt

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,26 +1,27 @@
+
 // Give the service worker access to Firebase Messaging.
-// Note that you can only use Firebase Messaging here. Other Firebase libraries are not available in the service worker.
-importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
+// Note: 'compat' libraries are used here for simpler initialization in a single file.
+importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js');
 
 // Initialize the Firebase app in the service worker by passing in
 // your app's Firebase config object.
-// You can find these values in your Firebase Console (Project Settings > General).
 firebase.initializeApp({
+  projectId: "studio-874039458-d0447",
+  appId: "1:196367644911:web:74bd118b2cb442b1dc031a",
   apiKey: "AIzaSyD_wEf8tEk9ZffJfUnLI7ndITSt5p05FoU",
   authDomain: "studio-874039458-d0447.firebaseapp.com",
-  projectId: "studio-874039458-d0447",
   storageBucket: "studio-874039458-d0447.appspot.com",
-  messagingSenderId: "196367644911",
-  appId: "1:196367644911:web:74bd118b2cb442b1dc031a"
+  messagingSenderId: "196367644911"
 });
 
 // Retrieve an instance of Firebase Messaging so that it can handle background messages.
 const messaging = firebase.messaging();
 
+// Optional: Handle background messages
 messaging.onBackgroundMessage((payload) => {
   console.log('[firebase-messaging-sw.js] Received background message ', payload);
-  // Customize notification here
+  
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,

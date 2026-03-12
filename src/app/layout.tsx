08ab67e907d201e0d13f3603b5d44e7168bdd2a0
emmanuel nduwa <drnduwa@gmail.com>
2026-03-12 18:38:11 +0000

    Try fixing this error: `Unhandled Runtime Error: ChunkLoadError: Loading

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -68,8 +68,8 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased">
+        <ChunkErrorReloader />
         <FirebaseClientProvider>
-          <ChunkErrorReloader />
           {children}
         </FirebaseClientProvider>
         <Toaster />

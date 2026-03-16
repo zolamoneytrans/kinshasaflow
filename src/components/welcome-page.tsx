2ca7c5cd97d21150372a54cb8ad0936f67c7e6c7
emmanuel nduwa <drnduwa@gmail.com>
2026-03-16 10:53:39 +0000

    it is not visible

--- src/components/welcome-page.tsx
+++ src/components/welcome-page.tsx
@@ -1,4 +1,3 @@
-
 'use client';
 
 import Link from 'next/link';
@@ -78,8 +77,8 @@ export default function WelcomePage() {
             <div className="absolute top-[20%] right-[-10%] -z-10 h-[500px] w-[500px] rounded-full bg-accent/15 blur-[130px]"></div>
             <div className="absolute bottom-[-10%] left-[10%] -z-10 h-[600px] w-[600px] rounded-full bg-blue-400/20 blur-[150px]"></div>
 
-            {/* Arrière-plan personnalisé (Echangeur) placé derrière le texte principal */}
-            <div className="absolute top-[12%] left-1/2 -translate-x-1/2 -z-20 w-full max-w-5xl h-[550px] opacity-[0.07] pointer-events-none blur-[2px]">
+            {/* Arrière-plan personnalisé (Echangeur) placé derrière le texte principal - Visibilité améliorée */}
+            <div className="absolute top-[8%] left-1/2 -translate-x-1/2 -z-10 w-full max-w-6xl h-[650px] opacity-20 pointer-events-none">
                 <Image 
                     src={echangeurBg} 
                     alt="Echangeur Kinshasa Background" 

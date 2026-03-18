a90aff3299b5073ca550303a045fb31d86dd7d17
emmanuel nduwa <drnduwa@gmail.com>
2026-03-18 16:55:31 +0000

    Try fixing this error: `Runtime Error: Invalid src prop (https://unsplas

--- next.config.js
+++ next.config.js
@@ -24,6 +24,12 @@ const nextConfig = {
       },
       {
         protocol: 'https',
+        hostname: 'unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
         hostname: 'picsum.photos',
         port: '',
         pathname: '/**',

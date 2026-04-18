d1e447e98992a792e4bff8229b9c764f44243d64
emmanuel nduwa <drnduwa@gmail.com>
2026-04-18 20:45:52 +0000

    I would like to publish the app on Playstore

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -82,7 +82,7 @@ export default function RootLayout({
       <head>
         <meta name="application-name" content="Kinshasa Flow" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="apple-mobile-web-app-title" content="Kinshasa Flow" />
         <meta name="format-detection" content="telephone=no" />
         <meta name="mobile-web-app-capable" content="yes" />
